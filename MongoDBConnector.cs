a810ec3b78892a15fac029cf320861da0d35bb21
Khireec <khireecarr479@gmail.com>
2025-09-26 13:46:37 -0400

    Chore declare ping method in MongoDBConnector

--- MongoDBConnector.cs
+++ MongoDBConnector.cs
@@ -12,19 +12,10 @@ namespace MongoDBConnectorLib
             _client = new MongoClient(connectionString);
         }
 
+        =
         public bool Ping()
         {
-            try
-            {
-                var db = _client.GetDatabase("admin");
-                var command = new BsonDocument("ping", 1);
-                db.RunCommand<BsonDocument>(command);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
+            
+            throw new NotImplementedException();
         }
-    }
 }
