32ea81abc321db3603225567a1d8670930426022
Khireec <khireecarr479@gmail.com>
2025-09-26 13:32:24 -0400

    Added MongoDBConnector with constuctor and ping

--- MongoDBConnector.cs
+++ MongoDBConnector.cs
@@ -1,7 +1,30 @@
-﻿namespace MongoDBConnector
-{
-    public class Class1
-    {
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 
+namespace MongoDBConnectorLib
+{
+    public class MongoDBConnector
+    {
+        private readonly MongoClient _client;
+
+        public MongoDBConnector(string connectionString)
+        {
+            _client = new MongoClient(connectionString);
+        }
+
+        public bool Ping()
+        {
+            try
+            {
+                var db = _client.GetDatabase("admin");
+                var command = new BsonDocument("ping", 1);
+                db.RunCommand<BsonDocument>(command);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
