94eec3b4bade2c50b753e105a9c9dd4b89dca3ac
Khireec <khireecarr479@gmail.com>
2025-09-26 14:02:45 -0400

    Added test cases for MongoDB

--- MongoDBConnector.cs
+++ MongoDBConnector.cs
@@ -12,10 +12,19 @@ namespace MongoDBConnectorLib
             _client = new MongoClient(connectionString);
         }
 
-        =
         public bool Ping()
         {
-            
-            throw new NotImplementedException();
+            try
+            {
+                var db = _client.GetDatabase("admin");
+                var command = new BsonDocument("ping", 1);
+                db.RunCommand<BsonDocument>(command);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
+    }
 }
---
+++ MongoDBConnector.tests.cs
@@ -0,0 +1,35 @@
+using Xunit;
+using MongoDBConnectorLib;
+using Testcontainers.MongoDb;
+using System.Threading.Tasks;
+
+namespace MongoDBConnectorLib.Tests
+{
+    public class MongoDBConnectorTests : IAsyncLifetime
+    {
+        private readonly MongoDbContainer _mongoDbContainer;
+
+        public MongoDBConnectorTests()
+        {
+            _mongoDbContainer = new MongoDbBuilder().Build();
+        }
+
+        public async Task InitializeAsync() => await _mongoDbContainer.StartAsync();
+
+        public async Task DisposeAsync() => await _mongoDbContainer.DisposeAsync();
+
+        [Fact]
+        public void Ping_ShouldReturnTrue_WhenMongoDBIsRunning()
+        {
+            var connector = new MongoDBConnector(_mongoDbContainer.GetConnectionString());
+            Assert.True(connector.Ping());
+        }
+
+        [Fact]
+        public void Ping_ShouldReturnFalse_WhenMongoDBIsInvalid()
+        {
+            var connector = new MongoDBConnector("mongodb://invalid:27017");
+            Assert.False(connector.Ping());
+        }
+    }
+}
